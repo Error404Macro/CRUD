8cc3f68b9eef730bcfd49cc354108b8b0e5d2fa1
Error <ya.gaukler@gmail.com>
2023-12-21 13:34:34 +0300

    userService has been added

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import web.Service.UserService;
-import web.dao.UserDAOImpl;
+import web.Service.UserServiceImpl;
 import web.models.User;
 
 import javax.validation.Valid;
--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -10,31 +10,28 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@Transactional
 public class UserDAOImpl implements UserDao {
 
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional(readOnly = true)
     public List<User> index() {
         return entityManager.createQuery("select user from User user", User.class)
                 .getResultList();
 
     }
 
-    @Transactional(readOnly = true)
     public User show(int id) {
         return entityManager.find(User.class, id);
     }
 
-    @Transactional
+
     public void save(User user) {
         entityManager.persist(user);
         entityManager.flush();
     }
 
-    @Transactional
+
     public void update(int id, User updatedUser) {
         User userToBeUpdated = entityManager.find(User.class, id);
         if (userToBeUpdated != null) {
@@ -43,7 +40,7 @@ public class UserDAOImpl implements UserDao {
         }
     }
 
-    @Transactional
+
     public void delete(int id) {
         User user = entityManager.find(User.class, id);
         if (user != null) {
